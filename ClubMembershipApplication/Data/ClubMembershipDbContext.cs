038dcd78a09354ed206045c1c281cdc637e96684
Yama <yama.spec@yahoo.es>
2023-10-12 11:47:20 +1100

    Add project files.

---
+++ ClubMembershipApplication/Data/ClubMembershipDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ClubMembershipApplication.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ClubMembershipApplication.Data
+{
+    public class ClubMembershipDbContext : DbContext
+    {
+        public ClubMembershipDbContext(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer()
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ ClubMembershipApplication/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClubMembershipApplication
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}

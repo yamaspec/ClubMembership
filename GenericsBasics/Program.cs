72c8736f3b3a403d19d1df6428e9963e864a73ee
Yama <yama.spec@yahoo.es>
2023-10-31 14:46:21 +1100

    Generics - Basics

---
+++ GenericsBasics/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GenericsBasics
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Salaries salaries = new();
+
+            // Non-Generic list requires unboxing
+            // ----------------------------------
+            // ArrayList salaryList = salaries.GetSalaries();
+            // double salary = (double)salaryList[1];
+            // salary = salary + (salary * 0.02);
+
+            // Generic list is easier and simpler
+            // ----------------------------------
+            List<float> salaryList = salaries.GetSalaries();
+            float salary = salaryList[1];
+            salary = salary + (salary * 0.02f);
+
+            Console.WriteLine(salary);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GenericsBasics/Salaries.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace GenericsBasics
+{
+    public class Salaries
+    {
+        // private ArrayList _salaryList = new();   // None Generic list: data type could be misinterpreted.
+        private List<float> _salaryList = new();
+
+        public Salaries()
+        {
+            // The type within the angled brackets forces to enter the right data type.
+            // Entering 60000.34 would throw a compile error (CS1503: Cannot convert double to float).
+            _salaryList.Add(60000.34f);
+            _salaryList.Add(40000.51f);
+            _salaryList.Add(20000.23f);
+        }
+
+        public List<float> GetSalaries()
+        {
+            return _salaryList;
+        }
+    }
+}

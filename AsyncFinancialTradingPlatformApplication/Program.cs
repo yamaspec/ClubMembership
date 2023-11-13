e907a92e7ff57c1678f2d587d76215d6b2b2bad6
Yama <yama.spec@yahoo.es>
2023-11-13 19:46:41 +1100

    Async - CPU bound ops example - Task.Run()

---
+++ AsyncFinancialTradingPlatformApplication/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsyncFinancialTradingPlatformApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Method name: Main, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            StockMarketTechnicalAnalysisData stockMarketTechnicalAnalysisData = new("STKZA", new DateTime(2020, 11, 30), new DateTime(2023, 11, 30));
+
+            DateTime dateTimeStartProcess = DateTime.Now;
+
+            List<decimal[]> data = new();
+            data.Add(stockMarketTechnicalAnalysisData.GetOpeningPrices());
+            data.Add(stockMarketTechnicalAnalysisData.GetClosingPrices());
+            data.Add(stockMarketTechnicalAnalysisData.GetPriceHighs());
+            data.Add(stockMarketTechnicalAnalysisData.GetPriceLows());
+            data.Add(stockMarketTechnicalAnalysisData.CalculateStockastics());
+            data.Add(stockMarketTechnicalAnalysisData.CalculateFastMovingAverage());
+            data.Add(stockMarketTechnicalAnalysisData.CalculateSlowMovingAverage());
+            data.Add(stockMarketTechnicalAnalysisData.CalculateUpperBoundBollingerBand());
+            data.Add(stockMarketTechnicalAnalysisData.CalculateLowerBoundBollingerBand());
+
+            DateTime dateTimeEndProcess = DateTime.Now;
+            TimeSpan timeSpan = dateTimeEndProcess.Subtract(dateTimeStartProcess);
+            // All Thread Ids are = 1 and took 37 seconds.
+            Console.WriteLine($"Time for synchronous operations to complete took {timeSpan.Seconds} {(timeSpan.Seconds > 1 ? "seconds" : "second")}.");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            dateTimeStartProcess = DateTime.Now;
+            List<Task<decimal[]>> tasks = new();
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.GetOpeningPrices()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.GetClosingPrices()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceHighs()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.GetPriceLows()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.CalculateStockastics()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.CalculateFastMovingAverage()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.CalculateSlowMovingAverage()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.CalculateUpperBoundBollingerBand()));
+            tasks.Add(Task.Run(() => stockMarketTechnicalAnalysisData.CalculateLowerBoundBollingerBand()));
+
+            Task.WaitAll(tasks.ToArray());
+            data = new();
+            data.Add(tasks[0].Result);
+            data.Add(tasks[1].Result);
+            data.Add(tasks[2].Result);
+            data.Add(tasks[3].Result);
+            dateTimeEndProcess = DateTime.Now;
+            timeSpan = dateTimeEndProcess.Subtract(dateTimeStartProcess);
+            // All Thread Ids are different among them and took 10 second.
+            Console.WriteLine($"Time for Asynchronous operations to complete took {timeSpan.Seconds} {(timeSpan.Seconds > 1 ? "seconds" : "second")}.");
+            Console.WriteLine();
+
+            DisplayDataOnChart( data );
+            Console.ReadKey();
+
+        }
+
+        public static void DisplayDataOnChart(List<decimal[]> data)
+        {
+            Console.WriteLine("Several charts displayed on the screen");
+        }
+    }
+}
---
+++ AsyncFinancialTradingPlatformApplication/StockMarketTechnicalAnalysisData.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Threading;
+
+namespace AsyncFinancialTradingPlatformApplication
+{
+    public class StockMarketTechnicalAnalysisData
+    {
+        public StockMarketTechnicalAnalysisData(string stockSymbol, DateTime dateTimeStart, DateTime dateTimeEnd)
+        {
+            // Code here gets the stock market data from remote server.
+        }
+
+        public decimal[] GetOpeningPrices()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(GetOpeningPrices)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep( 1000 ); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] GetClosingPrices()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(GetClosingPrices)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] GetPriceHighs()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(GetPriceHighs)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] GetPriceLows()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(GetPriceLows)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(1000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        // Overboard or over sold => buy/sell.
+        public decimal[] CalculateStockastics()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(CalculateStockastics)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(10000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] CalculateFastMovingAverage()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(CalculateFastMovingAverage)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(6000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] CalculateSlowMovingAverage()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(CalculateSlowMovingAverage)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(7000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] CalculateUpperBoundBollingerBand()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(CalculateUpperBoundBollingerBand)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+
+        public decimal[] CalculateLowerBoundBollingerBand()
+        {
+            decimal[] data = new decimal[] { };
+            Console.WriteLine($"Method name: {nameof(CalculateLowerBoundBollingerBand)}, ThreadId: {Thread.CurrentThread.ManagedThreadId}");
+            Thread.Sleep(5000); // Represent the time it takes for the operation to complete.
+            return data;  // Data variable would contain real decimal data.
+        }
+    }
+}

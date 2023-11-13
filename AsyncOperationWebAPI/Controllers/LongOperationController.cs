069788c070511bda65e35b9761a814b145ec5f3d
Yama <yama.spec@yahoo.es>
2023-11-13 15:57:12 +1100

    TAP - Introduction

---
+++ AsyncOperationWebAPI/Controllers/LongOperationController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace AsyncOperationWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class LongOperationController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            await Task.Delay(5000);
+            return Content("Web API Long Operation Completed");
+        }
+    }
+}
